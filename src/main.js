917b2f0766cf396021d5650c2843aebd5ac7e918
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-11-30 07:36:27 +0100

    [IDM_IHM][Configuration][Dependencies] added import for tachyons and VideoGen component to router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+/* eslint-disable import/first,import/no-webpack-loader-syntax */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+// eslint-disable-next-line import/first
 import App from './App';
+import '!style-loader!css-loader!tachyons/css/tachyons.css';
 import router from './router';
 
 Vue.config.productionTip = false;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import vgenfront from '@/components/VideoGen';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'VideoGen',
+      component: vgenfront,
     },
   ],
 });
