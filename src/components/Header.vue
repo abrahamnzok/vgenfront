0f2966568d1d9baddbdfdecaac68788b62674051
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-12-01 12:39:45 +0100

    [IDM_IHM][Configuration][Components] Created a header component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="bg-gold sans-serif">
+    <div class="mw9 center pa1 pt3-ns ph4-l">
+      <h3 class="f3 f1-m f-headline-l measure-narrow lh-title mv0">
+        <span class="bg-black-90 lh-copy white pa1 tracked-tight">
+          Videogen Platform
+        </span>
+      </h3>
+      <h5 class="f6 ttu tracked black-80">By Adam MUBANZO</h5>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+
+</style>
